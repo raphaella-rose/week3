8b8d54b8899768d4814f5f6ce0f8b50abec15d32
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-05-31 11:41:37 +0100

    updated get to post method

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,18 @@ get '/' do
   'hello, world!'
 end
 
-get '/cat' do
+get '/random_cat' do
   @cat_names = ["Amigo", "Misty", "Almond"].sample()
   erb(:index)
 end
+
+post '/named_cat' do
+  p params
+  @cat_names = params[:name]
+  erb(:cat_reveal)
+end
+
+get '/name_cat' do
+  erb(:index)
+end
+
